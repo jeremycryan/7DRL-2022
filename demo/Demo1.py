88471f9963b3e31719051e7f54971454c1c4921e
jeremycryan <jeremychristopherryan@gmail.com>
2022-03-02 18:43:30 -0600

    Update tile rule code

--- demo/Demo1.py
+++ demo/Demo1.py
@@ -5,6 +5,7 @@ from lib.Map import Map
 from lib.GridEntity import GridEntity
 from lib.Sprite import StaticSprite
 from lib.Settings import Settings
+import random
 
 
 class Player(GridEntity):
@@ -28,21 +29,104 @@ class Player(GridEntity):
                 if event.key == pygame.K_d:
                     self.move(x=1)
 
-class Tile(GridEntity):
+
+class Wall(GridEntity):
     def __init__(self, position=(0, 0)):
         super().__init__(position)
+        self.solid = True
 
     def load_sprite(self):
         surf = ImageHandler.load("images/tileset.png")
         tw = 64  # tile width
-        self.add_grid_rule(StaticSprite(surf,rect=(0, 0, tw, tw)), ("SSS","S@S","SS."))
-        self.add_grid_rule(StaticSprite(surf,rect=(tw, 0, tw, tw)), ("SSS","S@S","?.?"))
-        self.add_grid_rule(StaticSprite(surf,rect=(2*tw, 0, tw, tw)), ("SSS","S@S",".SS"))
-        self.add_grid_rule(StaticSprite(surf,rect=(3*tw, 0, tw, tw)), ("?SS",".@S","?S."))
-        self.add_grid_rule(StaticSprite(surf,rect=(4*tw, 0, tw, tw)), ("SSS","S@S",".S."))
-        self.add_grid_rule(StaticSprite(surf,rect=(4*tw, 0, tw, tw)), ("SS?","S@.",".S?"))
-        # TODO rest of rules
 
+        # Big donut
+        self.add_grid_rule(StaticSprite(surf, rect=(0, 0, tw, tw)), ("SSS", "S@S", "SS."))
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, 0, tw, tw)), ("SSS", "S@S", "?.?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(2*tw, 0, tw, tw)), ("SSS", "S@S", ".SS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(0, tw, tw, tw)), ("SS?", "S@.", "SS?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(2*tw, tw, tw, tw)), ("?SS", ".@S", "?SS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(0, 2*tw, tw, tw)), ("SS.", "S@S", "SSS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, 2*tw, tw, tw)), ("?.?", "S@S", "SSS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(2*tw, 2*tw, tw, tw)), (".SS", "S@S", "SSS"))
+
+        # Big vertical waffle
+        self.add_grid_rule(StaticSprite(surf, rect=(3*tw, 0, tw, tw)), ("?SS", ".@S", "?S."))
+        self.add_grid_rule(StaticSprite(surf, rect=(4*tw, 0, tw, tw)), ("SSS", "S@S", ".S."))
+        self.add_grid_rule(StaticSprite(surf, rect=(5*tw, 0, tw, tw)), ("SS?", "S@.", ".S?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(3*tw, tw, tw, tw)), ("?S.", ".@S", "?S."))
+        self.add_grid_rule(StaticSprite(surf, rect=(4*tw, tw, tw, tw)), (".S.", "S@S", ".S."))
+        self.add_grid_rule(StaticSprite(surf, rect=(5*tw, tw, tw, tw)), (".S?", "S@.", ".S?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(3*tw, 2*tw, tw, tw)), ("?S.", ".@S", "?SS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(4*tw, 2*tw, tw, tw)), (".S.", "S@S", "SSS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(5*tw, 2*tw, tw, tw)), (".S?", "S@.", "SS?"))
+
+        # Big horizontal waffle
+        self.add_grid_rule(StaticSprite(surf, rect=(0, 3*tw, tw, tw)), ("?.?", "S@S", "SS."))
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, 3*tw, tw, tw)), ("?.?", "S@S", ".S."))
+        self.add_grid_rule(StaticSprite(surf, rect=(2*tw, 3*tw, tw, tw)), ("?.?", "S@S", ".SS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(0, 4*tw, tw, tw)), ("SS.", "S@S", "SS."))
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, 4*tw, tw, tw)), (".S.", "S@S", ".S."))
+        self.add_grid_rule(StaticSprite(surf, rect=(2*tw, 4*tw, tw, tw)), (".SS", "S@S", ".SS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(0, 5*tw, tw, tw)), ("SS.", "S@S", "?.?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, 5*tw, tw, tw)), (".S.", "S@S", "?.?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(2*tw, 5*tw, tw, tw)), (".SS", "S@S", "?.?"))
+
+        # Solid wall
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, tw, tw, tw)), ("SSS", "S@S", "SSS"), likelihood=0.5)
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, 4*tw, tw, tw)), ("SSS", "S@S", "SSS"))
+
+        # Small donut
+        self.add_grid_rule(StaticSprite(surf, rect=(5*tw, 3*tw, tw, tw)), ("?.?", ".@S", "?S."))
+        self.add_grid_rule(StaticSprite(surf, rect=(5*tw, 4*tw, tw, tw)), ("?S.", ".@S", "?.?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(6*tw, 3*tw, tw, tw)), ("?.?", "S@.", ".S?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(6*tw, 4*tw, tw, tw)), (".S?", "S@.", "?.?"))
+
+        # Small square
+        self.add_grid_rule(StaticSprite(surf, rect=(3*tw, 3*tw, tw, tw)), ("?.?", ".@S", "?SS"))
+        self.add_grid_rule(StaticSprite(surf, rect=(3*tw, 4*tw, tw, tw)), ("?SS", ".@S", "?.?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(4*tw, 3*tw, tw, tw)), ("?.?", "S@.", "SS?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(4*tw, 4*tw, tw, tw)), ("SS?", "S@.", "?.?"))
+
+        # Skinny vertical bit
+        self.add_grid_rule(StaticSprite(surf, rect=(6*tw, 0, tw, tw)), ("?.?", ".@.", "?S?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(6*tw, tw, tw, tw)), ("?S?", ".@.", "?S?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(6*tw, 2*tw, tw, tw)), ("?S?", ".@.", "?.?"))
+
+        # Skinny horizontal bit
+        self.add_grid_rule(StaticSprite(surf, rect=(0, 6*tw, tw, tw)), ("?.?", ".@S", "?.?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(tw, 6*tw, tw, tw)), ("?.?", "S@S", "?.?"))
+        self.add_grid_rule(StaticSprite(surf, rect=(2*tw, 6*tw, tw, tw)), ("?.?", "S@.", "?.?"))
+
+        # Single block
+        self.add_grid_rule(StaticSprite(surf, rect=(6*tw, 5*tw, tw, tw)), ("?.?", ".@.", "?.?"))
+
+        # Default tile in case we've missed something
+        self.add_grid_rule(StaticSprite(surf, rect=(0, 0, tw, tw)), ("@",))
+
+        # Use rules to find the most appropriate sprite
+        self.add_sprite(self.get_sprite_from_grid_rules())
+
+
+class Floor(Wall):
+    def __init__(self, position=(0, 0)):
+        super().__init__(position)
+        self.solid = False
+
+    def load_sprite(self):
+        surf = ImageHandler.load("images/tileset.png")
+        tw = 64  # tile width
+
+        rect = random.choice(
+            (
+                (3*tw, 5*tw, tw, tw),
+                (4*tw, 5*tw, tw, tw),
+                (5*tw, 5*tw, tw, tw),
+            )
+        )
+        self.add_sprite(StaticSprite(surf, rect=rect))
+
+    def draw(self, surface, offset=(0, 0)):
+        super().draw(surface, offset)
 
 
 class Game:
@@ -73,6 +157,20 @@ class Game:
         clock.tick(60)
         map = Map(20, 20)
         map.add_empty_layer(0)
+        floor_layer = map.add_empty_layer(1)
+
+        for x, y in floor_layer.cell_coordinates():
+            if random.random() < 0.4:
+                new_tile = Wall()
+            else:
+                new_tile = Floor()
+            floor_layer.add_to_cell(new_tile, x, y)
+            new_tile.move_to_grid_position(x, y)
+
+        for cell in floor_layer.populated_cells():
+            for tile in cell:
+                tile.load_sprite()
+
         player = Player((0, 0))
         map.add_to_cell(player, 2, 2, 0)
 
